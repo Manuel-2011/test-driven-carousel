bc9b02cee2f9883637b6073b9a6c23900870aa0d
Manuel <mosquera.manuel2011@gmail.com>
2021-04-20 16:17:30 -0500

    :bulb: Add example page

---
+++ example/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Carousel from '../src/Carousel';
+import slides from './slides';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+ReactDom.render(<Carousel slides={slides} />, container);
