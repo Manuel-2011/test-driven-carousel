860dfe76b0056d5ca320c3914f5d5aa9c092da32
Manuel <mosquera.manuel2011@gmail.com>
2021-04-22 13:40:11 -0500

    small change in data

--- example/slides.js
+++ example/slides.js
@@ -43,7 +43,7 @@ export default [
       '&ixid=eyJhcHBfaWQiOjIzODE4fQ&s=e836c604036680eeba5c77ebdb171c73',
   },
   {
-    description: 'New York City',
+    description: 'New York',
     attribution: getAttribution({
       name: 'Anthony DELANOIX',
       username: 'anthonydelanoix',
